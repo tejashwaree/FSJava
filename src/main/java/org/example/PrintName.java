da36f925ac6eeb47f418fe4dcb6d7c570b7bb4ae
Teju <tejumankar4321@gmail.com>
2024-09-25 22:33:19 +0530

    c1

---
+++ src/main/java/org/example/PrintName.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class PrintName
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Tejashwaree" );
+    }
+}
